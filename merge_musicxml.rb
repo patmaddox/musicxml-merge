66af0c600e5a66383f33b6cb20af2e805b9b24a2
Pat Maddox <pat@patmaddox.com>
2019-04-06 05:51:47 -0700

    inserted the basic components

--- merge_musicxml.rb
+++ merge_musicxml.rb
@@ -11,35 +11,88 @@ scores = [
 
 score = scores.shift
 
-id = score.at 'identification'
+score_partwise = score.at 'score-partwise'
 
 # add work info:
 # <score-partwise>
-id.add_previous_sibling <<-END
+score_partwise.prepend_child <<-END
   <work>
    <work-title>Combined Dorico Scores</work-title>
   </work>
 END
 
+metronome = (score / "part[id='P1']" / "measure").first.at("attributes").add_next_sibling(<<-END
+  <direction>
+   <direction-type>
+    <metronome>
+     <beat-unit>quarter</beat-unit>
+     <per-minute>50</per-minute>
+    </metronome>
+   </direction-type>
+  </direction>
+END
+.strip).first
+
+metronome.add_next_sibling(<<-END
+  <direction>
+   <direction-type>
+    <words>Section 1</words>
+   </direction-type>
+  </direction>
+END
+.strip)
+
 measure_offset = (score.at("part[id='P1']") / "measure").size
 
 # iterate over scores / parts
-scores.each do |s|
+scores.each_with_index do |s, i|
   # iterate over measures for each part
   # increment measure number for additional scores
+  new_score = true
+
   (s / "part").each do |part|
     # puts part.to_xml
     score_part = score.at("part[id='#{part['id']}']")
+
     (part / "measure").inject(measure_offset + 1) do |measure_number, measure|
       measure['number'] = measure_number
+
+      if new_score
+        # print new page:
+        # <measure number="3">
+        #   <print new-page="yes" />
+
+        measure.prepend_child '<print new-page="yes" />'
+        attributes = measure.at 'attributes'
+
+        metronome = attributes.add_next_sibling(<<-END
+          <direction>
+           <direction-type>
+            <metronome>
+             <beat-unit>quarter</beat-unit>
+             <per-minute>125</per-minute>
+            </metronome>
+           </direction-type>
+          </direction>
+        END
+        .strip).first
+        $metronome = metronome
+        # attributes.add_next_sibling metronome
+        metronome.add_next_sibling(<<-END
+          <direction>
+           <direction-type>
+            <words>Section #{i + 2}</words>
+           </direction-type>
+          </direction>
+        END
+        .strip)
+        new_score = false
+      end
+
       score_part.add_child measure
       measure_number + 1
     end
   end
-
-  # print new page:
-  # <measure number="3">
-  #   <print new-page="yes" />
 
   # add metronome marker:
   # </attributes>
